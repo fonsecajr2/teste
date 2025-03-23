81bfa085549f020eaaa1a7b1f8bc4108fbafb793
fonsecajr2 <sheltonfonsecad@gmail.com>
2025-03-24 01:05:15 +0530

    profile picture attached sucessfully

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import logo from "./assets/logo10.png";
+import me from "./assets/profile.jpg";
 
 function App () {
   const[isOpen, setIsOpen] = useState(false);
 
   return(
-    <div className="min-h-screen w-screen flex flex-col bg-yellow-100">
+    <div className="min-h-screen w-full flex flex-col bg-yellow-100 overflow-x-hidden">
 
-      <nav className="fixed top-0 w-full z-50 p-2 shadow-md  ">
+      <nav className="fixed top-0 w-full z-50 p-2 shadow-md bg-yellow-100 ">
         <div className="container mx-auto flex justify-between items-center">
           <div>
             <img className="w-30 h-7.5" src={logo} alt="Logo" />
@@ -39,25 +40,30 @@ function App () {
         )}
       </nav>
 
-      <div className="pt-25">
-        <h1>Hi, This is Fonseca Junior Here</h1>
-        <h2>This is my Portifolio Website</h2>
-
-        <p>
-          Computer Science and Engineering (CSE) student with a strong interest in software development, cybersecurity, and advanced algorithms. Currently deepening my expertise in data structures such as Binary Search Trees and Linked Lists, along with programming in C++, focusing on designing efficient and scalable solutions.
-        </p>
+      <div className="flex-1 min-h-screen px-5 flex flex-col justify-center items-center ">
+        <div className="flex justify-center items-center w-full max-w-5xl mt-4 gap-x-20">
+          <div className="w-1/2 pr-8 text-left">
+            <h1 className="text-2xl font-bold">Hi, This is Fonseca Junior Here</h1>
+            <p className="">
+              Computer Science and Engineering (CSE) student with a strong interest in software development, cybersecurity, and advanced algorithms. Currently deepening my expertise in data structures such as Binary Search Trees and Linked Lists, along with programming in C++, focusing on designing efficient and scalable solutions.
+            </p>
+          </div>
+          <div className="w-1/2 flex justify-center">
+            <img src={me} alt="Image" className="rounded-full"/>
+          </div>
+        </div>
 
       </div>
 
-      <div>
+      <div className="min-h-screen">
         <h1>Skills Div</h1>
       </div>
 
-      <div>
+      <div className="min-h-screen">
         <h1>Certificates Div</h1>
       </div>
 
-      <div>
+      <div className="min-h-screen">
         <h1>Projects Div</h1>
       </div>
 
