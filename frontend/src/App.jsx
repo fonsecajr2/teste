523a8383cce940ffddd6d640da0007067796ec0d
fonsecajr2 <sheltonfonsecad@gmail.com>
2025-03-26 13:13:48 +0530

    skills list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -77,7 +77,7 @@ function App () {
 
       <div className="min-h-screen flex justify-center items-center relative">
 
-        <h1 className="text-3xl font-bold flex absolute top-11">My Skills</h1>
+        <h1 className="text-3xl font-bold flex absolute items-center top-11">My Skills</h1>
 
         <button className="custom-prev absolute left-0 z-10 bg-transparent text-yellow-600 p-6 rounded-full transition-transform duration-300 hover:scale-210">
           <ChevronLeft size={20} />
@@ -97,43 +97,190 @@ function App () {
           spaceBetween={30}
           slidesPerView={3}
           loop={true}
-          className="w-full h-screen"
+          className="w-full h-[60vh]"
           autoplay={{ delay: 5000, disableOnInteraction: false }}
         >
           
 
           <SwiperSlide>
-            <div className="flex justify-center items-center w-full h-screen">
-              <h1>Front-End Skills and Frameworks</h1>
+            <div className="flex justify-center items-center w-full h-full">
+              <div className="w-[25vw] h-[60vh] bg-white rounded-3xl shadow-md p-4 flex justify-center items-center">
+                <h1 className="flex absolute top-10">Front-End Skills and Frameworks</h1>
+                <ul>
+                  <li>
+                    <h2 className="text-lg font-bold">Languages</h2>
+                    <ul>
+                      <li>HTML</li>
+                      <li>JavaScript</li>
+                      <li>TypeScript</li>
+                    </ul>
+                  </li>
+                  <ul>
+                    <li>
+                      <h2 className="text-lg font-bold">Frameworks</h2>
+                      <ul>
+                        <li>ReactJs</li>
+                        <li>NextJs</li>
+                      </ul>
+                    </li>
+                  </ul>
+                  <li>
+                    <h2 className="text-lg font-bold">Tools</h2>
+                    <ul>
+                      <li>Tailwind CSS</li>
+                      <li>Bootstrap</li>
+                      <li>Redux</li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
           </SwiperSlide>
+
           <SwiperSlide>
-            <div className="flex justify-center items-center w-full h-screen">
-              <h1>Back-End Skills and Frameworks</h1>
+            <div className="flex justify-center items-center w-full h-full">
+              <div className="w-[25vw] h-[60vh] bg-white rounded-3xl shadow-md p-4 flex justify-center items-center">
+                <h1 className="flex absolute top-10">Back-End Skills and Frameworks</h1>
+                <ul>
+                  <li>
+                    <h2 className="text-lg font-bold">Languages</h2>
+                    <ul>
+                      <li>Node.js</li>
+                      <li>C++</li>
+                    </ul>
+                  </li>
+                  <li>
+                    <h2 className="text-lg font-bold">Frameworks</h2>
+                    <ul>
+                      <li>Express.Js</li>
+                      <li>Nest.js</li>
+                    </ul>
+                  </li>
+                  <li>
+                    <h2 className="text-lg font-bold">APIs & Autenticação</h2>
+                    <ul>
+                      <li>REST</li>
+                      <li>GraphQL</li>
+                      <li>JWT</li>
+                      <li>OAuth</li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
           </SwiperSlide>
+
           <SwiperSlide>
-            <div className="flex justify-center items-center w-full h-screen">
-              <h1>Database Skills and Tools</h1>
+            <div className="flex justify-center items-center w-full h-full">
+              <div className="w-[25vw] h-[60vh] bg-white rounded-3xl shadow-md p-4 flex justify-center items-center">
+                <h1 className="flex absolute top-10">DataBase Skills and Tools</h1>
+                <ul>
+                  <li>
+                    <h2 className="text-lg font-bold">SQL</h2>
+                    <ul>
+                      <li>MySQL</li>
+                      <li>PostgreSQL</li>
+                    </ul>
+                  </li>
+                  <li>
+                    <h2 className="text-lg font-bold">NoSQL</h2>
+                    <ul>
+                      <li>MongoDB</li>
+                    </ul>
+                  </li>
+                  <li>
+                    <h2 className="text-lg font-bold">ORM & Query Builders</h2>
+                    <ul>
+                      <li>Mongoose</li>
+                      <li>Prisma</li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
           </SwiperSlide>
+
           <SwiperSlide>
-            <div className="flex justify-center items-center w-full h-screen">
-              <h1>DevOps Skills</h1>
+            <div className="flex justify-center items-center w-full h-full">
+              <div className="w-[25vw] h-[60vh] bg-white rounded-3xl shadow-md p-4 flex justify-center items-center">
+                <h1 className="flex absolute top-10">DevOps Skills and Tools</h1>
+                <ul>
+                  <li>
+                    <h2 className="text-lg font-bold">CI/CD</h2>
+                    <ul>
+                      <li>GitHub Actions</li>
+                      <li>Jenkins</li>
+                    </ul>
+                  </li>
+                  <li>
+                    <h2 className="text-lg font-bold">Cloud Platforms</h2>
+                    <ul>
+                      <li>AWS (EC2, S3)</li>
+                      <li>Firebase</li>
+                    </ul>
+                  </li>
+                  <li>
+                    <h2 className="text-lg font-bold">Containerization</h2>
+                    <ul>
+                      <li>Docker</li>
+                      <li>Openshift</li>
+                      <li>Kubernetes</li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
           </SwiperSlide>
+
           <SwiperSlide>
-            <div className="flex justify-center items-center w-full h-screen">
-              <h1>CyberSecurity and Networking Skills</h1>
+            <div className="flex justify-center items-center w-full h-full">
+              <div className="w-[25vw] h-[60vh] bg-white rounded-3xl shadow-md p-4 flex justify-center items-center">
+                <h1 className="flex absolute top-10">CyberSecurity and Networks Skills</h1>
+                <ul>
+                  <li>
+                    <h2 className="text-lg font-bold">Networking</h2>
+                    <ul>
+                      <li>Firewalls</li>
+                      <li>VPN</li>
+                      <li>CCNA</li>
+                      <li>TCP/IP</li>
+                    </ul>
+                  </li>
+                  <li>
+                    <h2 className="text-lg font-bold">Web CyberSecurity & Pentesting</h2>
+                    <ul>
+                      <li>OWASP Top 10</li>
+                      <li>Burp Suit</li>
+                      <li>Metasploit</li>
+                    </ul>
+                  </li>
+                  <li>
+                    <h2 className="text-lg font-bold">CyberSecurity Tools</h2>
+                    <ul>
+                      <li>SIEM</li>
+                      <li>Nmap</li>
+                      <li>Wireshark</li>
+                      <li>Snort</li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
           </SwiperSlide>
+
           <SwiperSlide>
-            <div className="flex justify-center items-center w-full h-screen">
-              <h1>Soft Skills</h1>
+            <div className="flex justify-center items-center w-full h-full">
+              <div className="w-[25vw] h-[60vh] bg-white rounded-3xl shadow-md p-4 flex justify-center items-center relative">
+                <h1 className="flex absolute top-10">Soft Skills</h1>
+                <ul className="flex-row">
+                  <li>Communication</li>
+                  <li>Teamwork</li>
+                  <li>Problem Solving</li>
+                </ul>
+              </div>
             </div>
           </SwiperSlide>
         </Swiper>
-
       </div>
 
       
